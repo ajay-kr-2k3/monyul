e47ef1a473b41eb0708aa0556bbfc949246f7f62
AJ <ajaykumarsgrd@gmail.com>
2026-09-14 02:57:59 +0530

    Configure Github Pages deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "export",
+  basePath: "/monyul",
+  assetPrefix: "/monyul/",
+  trailingSlash: true,
 };
 
 export default nextConfig;
